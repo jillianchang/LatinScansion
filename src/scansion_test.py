e89a145f5eb1b5fbcb44b8198f0f722b43dd34ce
Jillian Chang <jillianchang15@gmail.com>
2021-08-18 09:24:38 -0400

    revised Aeneid 1 tests and removed diastole from the variable rule composition

--- src/scansion_test.py
+++ src/scansion_test.py
@@ -59,48 +59,32 @@ class ScansionTest(unittest.TestCase):
         self.assertFalse(line.defective)
 
     # Tests for Aeneid Book 1, for which the combined grammar fails to scan.
+
     def test_aen_1_247(self):
         text = "Hīc tamen ille urbem Patavī sēdēsque locāvit"
         line = self.scan_line(text, 247)
-        self.assertEqual(line.line_number, 247)
-        self.assertEqual(line.text, text)
-        self.assertEqual(
-            line.norm, "hīc tamen ille urbem patavī sēdēsque locāvit"
-        )
         self.assertEqual(
             line.pron, "hiːk tame nillurbẽː patawiː seːdeːskwe lokaːwit"
         )
+
     def test_aen_1_254(self):
         # MCL
         text = "Ollī subrīdēns hominum sator atque deōrum"
         line = self.scan_line(text, 254)
-        self.assertEqual(line.line_number, 254)
-        self.assertEqual(line.text, text)
-        self.assertEqual(
-            line.norm, "ollī subrīdēns hominum sator atque deōrum"
-        )
         self.assertEqual(
             line.pron, "olliː subriːdeːns hominũː sato ratkwe deoːrũː"
         )
+
     def test_aen_1_450(self):
         text = "Hōc prīmum in lūcō nova rēs oblāta timōrem"
         line = self.scan_line(text, 450)
-        self.assertEqual(line.line_number, 450)
-        self.assertEqual(line.text, text)
-        self.assertEqual(
-            line.norm, "hōc prīmum in lūcō nova rēs oblāta timōrem"
-        )
         self.assertEqual(
             line.pron, "hoːk priːmin luːkoː nowa reːs oblaːta timoːrẽː"
         )
+
     def test_aen_1_477(self):
         text = "lōra tenēns tamen; huic cervīxque comaeque trahuntur"
         line = self.scan_line(text, 477)
-        self.assertEqual(line.line_number, 477)
-        self.assertEqual(line.text, text)
-        self.assertEqual(
-            line.norm, "lōra tenēns tamen huic cervīxque comaeque trahuntur"
-        )
         self.assertEqual(
             line.pron, "loːra teneːns tame nujk kerwiːkskwe komajkwe trahuntur"
         )
@@ -108,33 +92,20 @@ class ScansionTest(unittest.TestCase):
     def test_aen_1_593(self):
         text = "argentum Pariusve lapis circumdatur aurō."
         line = self.scan_line(text, 593)
-        self.assertEqual(line.line_number, 593)
-        self.assertEqual(line.text, text)
         self.assertEqual(
-            line.norm, "argentum pariusve lapis circumdatur aurō"
+            line.pron, "argentũː pariuswe lapis kirkumdatu rawroː"
         )
-        self.assertEqual(
-            line.pron, "argentũː parjuswe lapis kirkumdatu rawroː"
-        )
+
     def test_aen_1_649(self):
         text = "et circumtextum croceō vēlāmen acanthō,"
         line = self.scan_line(text, 649)
-        self.assertEqual(line.line_number, 649)
-        self.assertEqual(line.text, text)
         self.assertEqual(
-            line.norm, "et circumtextum croceō vēlāmen acanthō"
+            line.pron, "et kirkumtekstũː krokeoː weːlaːme nakantoː"
         )
-        self.assertEqual(
-            line.pron, "et kirkumtekstũː kroceoː weːlaːme nakantoː"
-        )
+
     def test_aen_1_682(self):
         text = "nē quā scīre dolōs mediusve occurrere possit."
         line = self.scan_line(text, 682)
-        self.assertEqual(line.line_number, 682)
-        self.assertEqual(line.text, text)
-        self.assertEqual(
-            line.norm, "nē quā scīre dolōs mediusve occurrere possit"
-        )
         self.assertEqual(
             line.pron, "neː kwaː skiːre doloːs mediuswokkurrere possit"
         )
@@ -142,11 +113,6 @@ class ScansionTest(unittest.TestCase):
     def test_aen_1_697(self):
         text = "pallamque et pictum croceō vēlāmen acanthō."
         line = self.scan_line(text, 697)
-        self.assertEqual(line.line_number, 697)
-        self.assertEqual(line.text, text)
-        self.assertEqual(
-            line.norm, "pallamque et pictum croceō vēlāmen acanthō"
-        )
         self.assertEqual(
             line.pron, "pallãːkwet piktũː krokeoː weːlaːme nakantoː"
         )
