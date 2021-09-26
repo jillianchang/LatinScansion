2bfc50a5675534dfa798b7867a26385709661969
Kyle Gorman <kylebgorman@gmail.com>
2021-09-26 15:39:48 -0400

    Better var name

--- src/scan.py
+++ src/scan.py
@@ -18,7 +18,7 @@ def main(args: argparse.Namespace) -> None:
         source = stack.enter_context(open(args.input, "r"))
         sink = stack.enter_context(open(args.output, "w"))
         lines = [line.rstrip() for line in source]
-        scanned = scansion.scan_document(
+        document = scansion.scan_document(
             far["NORMALIZE"],
             far["PRONOUNCE"],
             far["VARIABLE"],
@@ -28,7 +28,8 @@ def main(args: argparse.Namespace) -> None:
             lines,
             args.name if args.name else os.path.normpath(args.input),
         )
-        text_format.PrintMessage(scanned, sink, as_utf8=True)
+        print(sink)
+        text_format.PrintMessage(document, sink, as_utf8=True)
 
 
 if __name__ == "__main__":
