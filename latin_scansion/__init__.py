eae80563e3f21e257d288589324d4813288fc959
Kyle Gorman <kylebgorman@gmail.com>
2021-10-04 10:54:27 -0400

    Makes code flake8 and black clean

--- latin_scansion/__init__.py
+++ latin_scansion/__init__.py
@@ -2,7 +2,14 @@
 
 import pkg_resources
 
-from latin_scansion.lib import *
+from latin_scansion.lib import read_document  # noqa: F401
+from latin_scansion.lib import write_document  # noqa: F401
+from latin_scansion.lib import scan_document  # noqa: F401
+from latin_scansion.lib import scan_verse  # noqa: F401
+from latin_scansion.lib import Document  # noqa: F401
+from latin_scansion.lib import Foot  # noqa: F401
+from latin_scansion.lib import Syllable  # noqa: F401
+from latin_scansion.lib import Verse  # noqa: F401
 
 __version__ = pkg_resources.get_distribution("latin_scansion").version
 __all__ = [
--- latin_scansion/lib/__init__.py
+++ latin_scansion/lib/__init__.py
@@ -1,8 +1,8 @@
-from latin_scansion.lib.scansion import scan_document
-from latin_scansion.lib.scansion import scan_verse
-from latin_scansion.lib.scansion_pb2 import Document
-from latin_scansion.lib.scansion_pb2 import Foot
-from latin_scansion.lib.scansion_pb2 import Syllable
-from latin_scansion.lib.scansion_pb2 import Verse
-from latin_scansion.lib.textproto import read_document
-from latin_scansion.lib.textproto import write_document
+from latin_scansion.lib.scansion import scan_document  # noqa: F401
+from latin_scansion.lib.scansion import scan_verse  # noqa: F401
+from latin_scansion.lib.scansion_pb2 import Document  # noqa: F401
+from latin_scansion.lib.scansion_pb2 import Foot  # noqa: F401
+from latin_scansion.lib.scansion_pb2 import Syllable  # noqa: F401
+from latin_scansion.lib.scansion_pb2 import Verse  # noqa: F401
+from latin_scansion.lib.textproto import read_document  # noqa: F401
+from latin_scansion.lib.textproto import write_document  # noqa: F401
