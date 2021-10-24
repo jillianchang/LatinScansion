778a7f72800c50a8133fdf1cd88399f0f355671a
Kyle Gorman <kylebgorman@gmail.com>
2021-10-24 01:16:07 -0400

    Fixes redef

--- latin_scansion/__init__.py
+++ latin_scansion/__init__.py
@@ -6,7 +6,6 @@ from .scansion_pb2 import Document
 from .scansion_pb2 import Foot
 from .scansion_pb2 import Syllable
 from .scansion_pb2 import Verse
-from .scansion_pb2 import Verse
 from .textproto import read_document
 from .textproto import write_document
 
